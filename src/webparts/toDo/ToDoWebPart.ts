23bc441b7470fc857e9498fc63383953fb7b1b6d
orcJeff <jeffrey@oldrivercreative.com>
2017-10-24 16:14:46 -0400

    CUSTOM CODE
    Many neat things

--- src/webparts/toDo/ToDoWebPart.ts
+++ src/webparts/toDo/ToDoWebPart.ts
@@ -1,10 +1,12 @@
+// All Imports
 import { Version } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
-  PropertyPaneDropdown,
   PropertyPaneCheckbox,
   PropertyPaneToggle,
+  PropertyPaneButton,
+  PropertyPaneButtonType,
   PropertyPaneTextField
 } from '@microsoft/sp-webpart-base';
 import { escape } from '@microsoft/sp-lodash-subset';
@@ -14,81 +16,110 @@ import {
   SPHttpClientResponse
 } from '@microsoft/sp-http';
 
+import {
+  Environment,
+  EnvironmentType
+} from '@microsoft/sp-core-library';
+
 import styles from './ToDoWebPart.module.scss';
 import * as strings from 'ToDoWebPartStrings';
 
+// ----------------------------------- //
+
+// define values for use later
 export interface IToDoWebPartProps {
-  description: string;
-  foobar: string;
-  test1: boolean;
-  test2: string;
-  test3: boolean;
+  todolist: string;
+  displaycount: string;
+  createlistbutton: string;
 }
 
+export interface SPtodolist {
+  value: SPtodolist[];
+}
+
+export interface SPtodolistparts {
+  Title: string;
+  Id: string;
+}
+
+// build web part
 export default class ToDoWebPartWebPart extends BaseClientSideWebPart<IToDoWebPartProps> {
 
+  // Get Method
+  private _fetchListItems(todolist: string): Promise<SPtodolist> {
+    return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + `/_api/web/lists/getbytitle('${todolist}')?`, SPHttpClient.configurations.v1)
+    .then((response: SPHttpClientResponse) =>
+    {return response.json();
+    });
+  }
+
+  // Render Method
   public render(): void {
     this.domElement.innerHTML = `
       <div class="${styles.toDo}">
-        <div class="${styles.container}">
-          <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
-            <div class="ms-Grid-col ms-lg10 ms-xl8 ms-xlPush2 ms-lgPush1">
-              <p class="ms-font-l ms-fontColor-white">${escape(this.properties.foobar)}</p>
-              <p class="ms-font-l ms-fontColor-white">${escape(this.properties.description)}</p>
-              <p class="ms-font-l ms-fontColor-white">${escape(this.properties.test2)}</p>
-
-              <a href="https://aka.ms/spfx" class="${styles.button}">
-                <span class="${styles.label}">Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+        <p>Potato</p>
       </div>`;
-  }
 
+      this._fetchListItems('Test').then((response) => {
+        console.log(response);
+      })
+  }
+  // Version
   protected get dataVersion(): Version {
     return Version.parse('1.0');
   }
-
+  //configure edit mode panel
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: 'A To-Do list web part.'
           },
           groups: [
-            {
-              groupName: strings.BasicGroupName,
+            {     // list name field
+              groupName: 'Enter List Name',
               groupFields: [
-              PropertyPaneTextField('description', {
-                label: 'Description'
-              }),
-              PropertyPaneTextField('foobar', {
-                label: 'Multi-line Text Field',
-                multiline: true
-              }),
-              PropertyPaneCheckbox('test1', {
-                text: 'Checkbox'
-              }),
-              PropertyPaneDropdown('test2', {
-                label: 'Dropdown',
-                options: [
-                  { key: '1', text: 'One' },
-                  { key: '2', text: 'Two' },
-                  { key: '3', text: 'Three' },
-                  { key: '4', text: 'Four' }
-                ]}),
-              PropertyPaneToggle('test3', {
-                label: 'Toggle',
-                onText: 'On',
-                offText: 'Off'
+              PropertyPaneTextField('todolist', {
+                label: 'To-Do List Name'
+              }), //display item field
+              PropertyPaneTextField('displaycount', {
+                deferredValidationTime: 400,
+                label: 'To-Do Item Display Limit',
+                maxLength: 2
+              }), // Button method
+              PropertyPaneButton('todolist', {
+                onClick: function(createlistbutton){
+                  createlistbutton = createlistbutton+"1";
+                  console.log(createlistbutton);
+                  return createlistbutton;
+                },
+                buttonType: PropertyPaneButtonType.Primary,
+                text: 'Create List'
               })
-            ]
-            }
+            ]}
           ]
         }
       ]
     };
   }
 }
+
+// Input Validation for display limit
+document.getElementsByClassName(".ms-TextField-field").addEventListener("change", function() {
+  let newvalue: string = document.getElementsByClassName(".ms-TextField-field").value
+  if (newvalue != ''){
+    let endstring: string = '';
+    let newarray: string[] = newvalue.split('');
+    for(let i of newarray) {
+      let checknum = Number(i);
+      if (checknum == NaN){
+        endstring = '';
+      }
+      if (checknum != NaN){
+        endstring = endstring+i;
+      }
+    }
+    document.getElementsByClassName(".ms-TextField-field").value = endstring;
+  }
+});
